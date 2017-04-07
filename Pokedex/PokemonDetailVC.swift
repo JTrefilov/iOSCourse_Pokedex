e696770f2202d14fd0f51b87cb75b49d290d5790
JTrefilov <jtrefilov@gmail.com>
2017-04-07 22:08:06 +0300

    Installed Alamofire

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
-    @IBOutlet weak var nameTest: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIDLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
     
     var chosenPokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameTest.text = chosenPokemon.name
+        nameLabel.text = chosenPokemon.name
+    }
+    
+    // Returning to main view controller
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 }
